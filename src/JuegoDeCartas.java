d90829f83e3ec697262b4a15364b29ce1af0977e
BRAYAN LEM <bixcoyacl@miumg.edu.gt>
2024-08-25 22:55:05 -0600

    Repaso juego de cartas

---
+++ src/JuegoDeCartas.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class JuegoDeCartas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Ingresar el numero de jugadores
+        System.out.print("Ingrese un numero de jugadores: ");
+        int n = sc.nextInt();
+
+        //Ingrese el numero de partidas
+        System.out.println("Inngrese el número de partidas o rondas");
+                int p  = sc.nextInt();
+
+        Juego juego = new Juego(n, p);
+        juego.jugar();
+    }
+}
